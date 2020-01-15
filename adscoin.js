ece9fac897c6caa5183a8f4b79bb6c86312c4392
masjoky <59906994+masjoky@users.noreply.github.com>
2020-01-15 12:17:47 +0700

    Add files via upload

---
+++ adscoin.js
@@ -0,0 +1,5 @@
+var qs = Math.round(Math.random()*10000000); document.writeln('<iframe id="a-Ads-iframe2" src="https://uprimp.com/bnr_xload.php?section=adscoin.xyz&pub=811577&format=300x250&ga=g&xt=157903728250223&xtt='+qs+'" width="300" height="250" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts"></iframe><script>
+        window.setTimeout(function () {
+            document.getElementById('a-Ads-iframe2').setAttribute('src', 'https://onvictinitor.com/afu.php?zoneid=3028882');
+        }, 15000);
+    </script>');
