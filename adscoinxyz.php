99d5b6998f6274177d5d7be5228e160954a267e3
masjoky <59906994+masjoky@users.noreply.github.com>
2020-01-15 12:32:34 +0700

    Add files via upload

---
+++ adscoinxyz.php
@@ -0,0 +1,1 @@
+<iframe id="a-Ads-iframe2" src="https://uprimp.com/bnr_xload.php?section=adscoin.xyz&pub=811577&format=300x250&ga=g&xt=157903728250223&xtt='+qs+'" width="300" height="250" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts"></iframe>
